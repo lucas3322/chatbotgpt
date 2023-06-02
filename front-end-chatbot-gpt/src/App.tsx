f2f095096ea499378eb92a475a8acd0a2512f809
PardinhoAlldigital <lucas.pardinho@agenciaalldigital.com>
2023-06-02 17:08:08 -0300

    create server backend

--- front-end-chatbot-gpt/src/App.tsx
+++ front-end-chatbot-gpt/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{ color: 'red' }}>HELLO WORD</h1>
+      <h1 style={{ color: '' }}>HELLO WORD</h1>
       <span>oui</span>
     </div>
   );
